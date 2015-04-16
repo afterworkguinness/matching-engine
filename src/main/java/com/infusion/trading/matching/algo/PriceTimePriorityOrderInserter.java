0a3c9e4b05cc0f5d44964cf7468f48642e640744
afterworkguinness <paul.churchward@gmail.com>
2015-04-15 22:14:53 -0400

    Refactoring

--- src/main/java/com/infusion/trading/matching/algo/PriceTimePriorityOrderInserter.java
+++ src/main/java/com/infusion/trading/matching/algo/PriceTimePriorityOrderInserter.java
@@ -13,50 +13,39 @@ public class PriceTimePriorityOrderInserter implements IOrderPlacementAlgorithm 
 	public int findPositionToPlaceInBook(List<LimitOrder> orders, LimitOrder order) {
 
 		int position = -1;
-		if (order.getSide() == OrderSide.BUY) {
-			if (orders.isEmpty() == false) {
-				for (int i = 0; i < orders.size(); i++) {
-					// Buy orders are ranked price descending
-					if (order.getLimitPrice() > orders.get(i).getLimitPrice()) {
-						position = i;
-						break;
-					}
-					else if (order.getLimitPrice() == orders.get(i).getLimitPrice()) {
-						/*
-						 * If the order matches the price of an existing order,
-						 * place it after the existing order
-						 */
-						position = i + 1;
-						break;
-					}
+
+		if (orders.isEmpty() == false) {
+			for (int i = 0; i < orders.size(); i++) {
+				if (compare(order, orders.get(i))) {
+					position = i;
+					break;
 				}
-			}
-			else {
-				position = 0;
+				else if (order.getLimitPrice() == orders.get(i).getLimitPrice()) {
+					/*
+					 * If the order matches the price of an existing order,
+					 * place it after the existing order
+					 */
+					position = i + 1;
+					break;
+				}
 			}
 		}
-		if (order.getSide() == OrderSide.SELL) {
-			if (orders.isEmpty() == false) {
-				for (int i = 0; i < orders.size(); i++) {
-					// Sell orders are ranked price ascending
-					if (order.getLimitPrice() < orders.get(i).getLimitPrice()) {
-						position = i;
-						break;
-					}
-					else if (order.getLimitPrice() == orders.get(i).getLimitPrice()) {
-						/*
-						 * If the order matches the price of an existing order,
-						 * place it after the existing order
-						 */
-						position = i + 1;
-						break;
-					}
-				}
-			}
-			else {
-				position = 0;
-			}
+		else {
+			position = 0;
 		}
 		return position;
+	}
+
+	private boolean compare(LimitOrder left, LimitOrder right) {
+
+		// Buy orders are ranked price descending
+		// Sell orders are ranked price ascending
+
+		if (left.getSide() == OrderSide.BUY) {
+			return left.getLimitPrice() > right.getLimitPrice();
+		}
+		else {
+			return left.getLimitPrice() < right.getLimitPrice();
+		}
 	}
 }
