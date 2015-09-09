39b8c4c4e2796f5ce7c6f56711ba12143ad1330b
afterworkguinness <paul.churchward@gmail.com>
2015-09-09 16:15:31 -0400

    Simple rest service to accept limit order setup (NOT HOOKED UP)

---
+++ web/src/main/java/com/infusion/trading/matching/domain/LimitOrderModel.java
@@ -0,0 +1,38 @@
+package com.infusion.trading.matching.domain;
+
+public class LimitOrderModel {
+
+	private String symbol;
+	private Double limitPrice;
+	private Integer quantity;
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public Double getLimitPrice() {
+		return limitPrice;
+	}
+
+	public void setLimitPrice(Double limitPrice) {
+		this.limitPrice = limitPrice;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "LimitOrderModel [symbol=" + symbol + ", limitPrice=" + limitPrice + ", quantity=" + quantity + "]";
+	}
+
+}
--- web/src/main/java/com/infusion/trading/matching/endpoint/TradeEndpoint.java
+++ web/src/main/java/com/infusion/trading/matching/endpoint/TradeEndpoint.java
@@ -1,7 +1,12 @@
 package com.infusion.trading.matching.endpoint;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import com.infusion.trading.matching.domain.LimitOrderModel;
 
 @Path("/trade")
 public class TradeEndpoint {
@@ -9,5 +14,12 @@ public class TradeEndpoint {
 	@GET
 	public String getFoo() {
 		return "Foo";
+	}
+
+	@POST
+	@Path("limit")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public void limitOrder(LimitOrderModel limitOrder) {
+		System.out.println(limitOrder);
 	}
 }
